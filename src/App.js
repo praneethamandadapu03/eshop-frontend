35f2dac740b220a46612a54d558160983428bc56
praneethamandadapu03 <praneetha.mandadapu@gmail.com>
2025-03-27 16:52:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 
-
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import ProductDetailPage from './pages/ProductDetailPage';
-import RegisterPage from './pages/RegisterPage';
-import HomePage from './pages/HomePage';
-import ProductPage from './pages/ProductPage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
-
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import ProductPage from './pages/ProductPage';
+import RegisterPage from './pages/RegisterPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/products' element={<ProductPage />} />
-      <Route path='/products/detail/:id' element={<ProductDetailPage />} />
-      <Route path='/about' element={<AboutPage />} />
-      <Route path='/contact' element={<ContactPage />} />
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/register' element={<RegisterPage />} />
-         
-       </Routes>
-       </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/products' element={<ProtectedRoute Component={ProductPage} />} />
+        <Route path='/products/detail/:id' element={<ProtectedRoute Component={ProductDetailPage} />} />
+        <Route path='/about' element={<ProtectedRoute Component={AboutPage} />} />
+        <Route path='/contact' element={<ProtectedRoute Component={ContactPage} />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
+}
 
 export default App;
